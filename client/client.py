fee8fec0fa95a77775adf7cc51e089564772f0d5
jennysj <je.sj@hotmail.com>
2016-03-04 15:06:46 +0100

    first client

--- client/client.py
+++ client/client.py
@@ -2,38 +2,49 @@
 import socket
 from MessageReceiver import MessageReceiver
 from MessageParser import MessageParser
+import json
+import select
 
 class Client:
     """
     This is the chat client class
     """
-
     def __init__(self, host, server_port):
         """
         This method is run when creating a new Client object
         """
-
+        self.host = 'localhost'
+        self.server_port = 9998
         # Set up the socket connection to the server
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # TODO: Finish init process with necessary code
+        self.messageParser = MessageParser()
         self.run()
 
     def run(self):
         # Initiate the connection to the server
         self.connection.connect((self.host, self.server_port))
+        while True:
+            while True:
+                self.connection.setblocking(0)
+                ready = select.select([self.connection], [], [], 0.4)
+                if ready[0]:
+                    data = self.connection.recv(1024)
+                    self.receive_message(data)
+                else:
+                    break
+            userInput = str(raw_input("Request: "))
+            content = str(raw_input("Content: "))
+            dictObject = {"request":userInput, "content":content}
+            jsonObject = json.dumps(dictObject, ensure_ascii=False)
+            self.send_payload(jsonObject)
 
-    def disconnect(self):
-        # TODO: Handle disconnection
-        pass
 
     def receive_message(self, message):
-        # TODO: Handle incoming message
-        pass
+        msg = self.messageParser.parse(message)
+        print msg
 
     def send_payload(self, data):
-        # TODO: Handle sending of a payload
-        pass
+        self.connection.send(data)
 
     # More methods may be needed!
 
